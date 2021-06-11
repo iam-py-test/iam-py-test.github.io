1002013b0d44aa97620634ca59840f122250846a
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-06-11 18:27:36 -0400

    Update checkup.js

--- js/checkup.js
+++ js/checkup.js
@@ -25,6 +25,7 @@ var main = async function(){
   catch(err){
     console.log("Error:",err)
   }
+  document.getElementById('norton').href = "https://safeweb.norton.com/report/show?url=" + domain
   
 }
 main().catch(console.error)
