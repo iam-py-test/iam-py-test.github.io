8543666d4ff9b1ce8e6df475e2779d35785d47c0
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-06-11 17:48:25 -0400

    Update checkup.js

--- js/checkup.js
+++ js/checkup.js
@@ -1,2 +1,16 @@
-
-var c = true
+var main = async function(){
+  fetch("https://raw.githubusercontent.com/iam-py-test/my_filters_001/main/Alternative%20list%20formats/antimalware_domains.txt").then(async function(req){
+    if(!new URL(location).searchParams.get("q")){
+      document.getElementById("my-am").textContent = 'Invalid domain'
+      return;
+    }
+    var text = await req.text().split("\n")
+    if(text.includes(new URL(location).searchParams.get("q"))){
+      document.getElementById('my-am').textContent = "Detected"
+    }
+    else{
+      document.getElementById('my-am').textContent = "Not detected"
+    }
+  })
+}
+main().catch(console.error)
