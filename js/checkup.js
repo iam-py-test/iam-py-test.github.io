c277d17332840157c280d21c492b04fa30a23eab
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-06-11 18:06:40 -0400

    Update checkup.js

--- js/checkup.js
+++ js/checkup.js
@@ -5,6 +5,7 @@ var main = async function(){
       return;
     }
     var text = await req.text()
+    
     if(text.includes(new URL(location).searchParams.get("q"))){
       document.getElementById('my-am').textContent = "Detected"
     }
@@ -17,8 +18,13 @@ var main = async function(){
   window.knowndomains = new Map([["google.com","Google"],["accounts.google.com","Google"],["google.org","Google"],["blog.google","Google"],["duckduckgo.com","DuckDuckGo"],["duck.com","DuckDuckGo"]])
   console.log(window.knowndomains)
   var domain = new URL(location).queryParams.get("q")
+  console.log(window.knowndomains,domain)
+  try{
   document.getElementById("known").textContent = (window.knowndomains.get(domain)||"Not known")
-  
+  }
+  catch(err){
+    console.log("Error:",err)
+  }
   
 }
 main().catch(console.error)
