9c3558f7868c6bfa408f45d973f65df75ca67b8b
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2022-01-28 11:06:22 -0500

    Create script.js

---
+++ dev/null/script.js
@@ -0,0 +1,11 @@
+/*
+This is the main script file for /dev/null
+
+*/
+window.listenEnter = true
+var inputElement = document.getElementById("input")
+inputElement.addEventListener(function(event){
+  if(event.key === "enter"){
+  }
+},"keydown")
+
