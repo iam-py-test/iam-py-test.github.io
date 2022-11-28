edd0ec7f91d328981ed91d06865386d7c9a569ba
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2022-11-28 17:20:22 -0500

    Remove favicon & new tool

---
+++ tools/undefang_urls/main.js
@@ -0,0 +1,28 @@
+"use strict";
+var undefang = function(url){
+    /*
+    this function takes in a defanged URL (hxxpx[://]example[.]com) or defanged domain (example[.]com) and returns an undefanged URL
+
+    */
+    var endresult = url
+    .replace("[.]",".")
+    .replace(/\[\:\/\/\]/g,"://")
+    .replace("hxxpx:/","https:/")
+    .replace("hxxp:/","http:/")
+
+   
+   console.log(url,endresult);
+   return endresult;
+
+}
+
+var processurls = function(){
+    var end = [];
+    var allurls = document.getElementById("urls").value.split("\n");
+    for(let i = 0; i < allurls.length;i++){
+        end.push(undefang(allurls[i]));
+    }
+    console.log(end,allurls);
+    document.getElementById("o").innerText = end.join("\n");
+}
+document.getElementById("urls").addEventListener("change",processurls);
