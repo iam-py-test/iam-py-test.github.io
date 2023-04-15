998d380ca34e3cdf1be7141eeee5856742f2185b
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2023-04-15 13:44:45 -0400

    Refine even more Ignore filters which filter by class Better handle network errors

--- tools/easylist-auto-detect/main.js
+++ tools/easylist-auto-detect/main.js
@@ -7,11 +7,19 @@ function pause(time){
 
 document.getElementById("run").addEventListener("click",async () => {
 const testingelm = document.getElementById("testingelm");
-const dataset = await (await fetch("https://raw.githubusercontent.com/easylist/easylist/master/easylist/easylist_general_hide.txt")).text();
+const resultelm = document.getElementById("result");
+try{
+    var dataset = await (await fetch("https://raw.githubusercontent.com/easylist/easylist/master/easylist/easylist_general_hide.txt")).text();
+}
+catch(err){
+    resultelm.textContent = "Failed to load EasyList list: " + err
+    return;
+}
 const datasetlines = dataset.split("\n");
 let allids = [];
+testingelm.textContent = "";
 for(let i = 0; i < datasetlines.length;i++){
-    if(datasetlines[i].startsWith("###") && datasetlines[i].includes(">") === false && datasetlines[i].includes("[") === false){
+    if(datasetlines[i].startsWith("###") && datasetlines[i].includes(">") === false && datasetlines[i].includes("[") === false && datasetlines[i].includes(".") === false){
         var tmpid = datasetlines[i].split("###")[1].replace(/:not\(.*\)/,"");
         allids.push(tmpid);
         var tmpelm = document.createElement("div");
@@ -29,5 +37,5 @@ for(let i2 = 0; i2 < allids.length;i2++){
         totaldetected += 1;
     }
 }
-document.getElementById("result").textContent = `${totaldetected} elements were hidden by a content blocker. ${(totaldetected/allids.length)*100}% likelyhood of EasyList generic cosmetic filtering`
+resultelm.textContent = `${totaldetected} elements were hidden by a content blocker. ${(totaldetected/allids.length)*100}% likelyhood of EasyList generic cosmetic filtering`
 })
