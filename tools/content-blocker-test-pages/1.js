b762efa87dea9c4f9030063660bf3db83c04fc85
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2023-08-23 18:00:42 -0400

    Add

--- tools/content-blocker-test-pages/1.js
+++ tools/content-blocker-test-pages/1.js
@@ -1,1 +1,9 @@
-fetch("/1.json")
+fetch("/1.json").then(async (r) => {
+    console.log(r);
+    console.log(await r.json())
+    console.log(await r.text())
+})
+var x = new XMLHttpRequest()
+x.open("GET", "1.json")
+x.onload = (r) => console.log(r)
+x.send()
