68fb619cfe0f958f706af6e0bb44ae1837a61076
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2024-01-25 18:17:18 -0500

    Some changes Introduce checking of error hashes into browser-info Turns out to be very boring, though I did find a new way to tell Firefox/Chromium apart.

--- tools/browser-info/main.js
+++ tools/browser-info/main.js
@@ -31,7 +31,48 @@ function get_canvas(){
     }
 }
 
+async function getHash(str, algo){
+    // https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#examples
+    let textEnc = new TextEncoder();
+    let encoded = textEnc.encode(str);
+    console.log(encoded, algo)
+    let hash_arr = await crypto.subtle.digest(algo, encoded);
+    let hash_byte_arr = Array.from(new Uint8Array(hash_arr));
+    let hash_hex = hash_byte_arr
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
+    return hash_hex;
+}
+
 const FUNC_TO_TEST = ["alert", "print", "open", "close", "atob", "btoa", "prompt"];
+const ERROR_TO_TEST = ["InternalError", "SyntaxError", "RangeError", "ReferenceError", "TypeError", "URIError", "Error", "DOMException", "EvalError", "AggregateError"];
+
+(async function(){
+    let error_hash_elm = document.getElementById("error_hashes");
+    ERROR_TO_TEST.forEach(async (err_name) => {
+        if(!window[err_name]){
+            let errordata_elm = document.createElement("p");
+            errordata_elm.textContent = `${err_name}: Error not supported`
+            error_hash_elm.appendChild(errordata_elm);
+            return;
+        }
+        try{
+            throw new window[err_name]("Browsers hid the facts");
+        }
+        catch(error_data){
+            if(error_data instanceof window[err_name] === false){
+                let errordata_elm = document.createElement("p");
+                errordata_elm.textContent = `${err_name}: Failed to check`
+                error_hash_elm.appendChild(errordata_elm);
+                return;
+            };
+            let errdata_hash = await getHash(new String(error_data), "SHA-256");
+            let errordata_elm = document.createElement("p");
+            errordata_elm.textContent = `${err_name}: ${errdata_hash}`
+            error_hash_elm.appendChild(errordata_elm);
+        }
+    })
+})()
 
 var getElmById = document.getElementById.bind(document);
 var createElm = document.createElement.bind(document)
