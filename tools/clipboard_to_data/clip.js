d98e43efa5a58429c2c9bf5847081019d4011d19
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2024-02-06 16:14:09 -0500

    Whoops

--- tools/clipboard_to_data/clip.js
+++ tools/clipboard_to_data/clip.js
@@ -2,7 +2,7 @@ window.onpaste = (e) => {
     let file = e.clipboardData.files[0];
     if(!file){return};
     const reader = new FileReader();
-    reader.addEventListener(function(){
+    reader.addEventListener("load", function(){
         document.getElementById("out").textContent = reader.re
     })
     reader.readAsDataURL(file);
